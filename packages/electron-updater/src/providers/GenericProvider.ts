504660ec45ff1d3451b30ab195fa4bc62a49553c
haofeng.zhao <haofeng.zhao@leyantech.com>
2019-03-11 15:01:46 +0800

    add httpdns support

--- packages/electron-updater/src/providers/GenericProvider.ts
+++ packages/electron-updater/src/providers/GenericProvider.ts
@@ -1,7 +1,12 @@
-import { GenericServerOptions, HttpError, newError, UpdateInfo } from "builder-util-runtime"
+import { CancellationToken, GenericServerOptions, HttpError, newError, UpdateInfo, WindowsUpdateInfo } from "builder-util-runtime"
 import { AppUpdater } from "../AppUpdater"
 import { getChannelFilename, newBaseUrl, newUrlFromBase, Provider, ResolvedUpdateFileInfo } from "../main"
 import { parseUpdateInfo, ProviderRuntimeOptions, resolveFiles } from "./Provider"
+import { dnsResolverManager, HttpDnsURL } from 'electron-httpdns'
+import { OutgoingHttpHeaders } from "http"
+
+
+
 
 export class GenericProvider extends Provider<UpdateInfo> {
   private readonly baseUrl = newBaseUrl(this.configuration.url)
@@ -14,13 +19,26 @@ export class GenericProvider extends Provider<UpdateInfo> {
     const result = this.updater.channel || this.configuration.channel
     return result == null ? this.getDefaultChannelName() : this.getCustomChannelName(result)
   }
+  private ipCache: { [index: string]: string } = {}
+
 
   async getLatestVersion(): Promise<UpdateInfo> {
     const channelFile = getChannelFilename(this.channel)
-    const channelUrl = newUrlFromBase(channelFile, this.baseUrl, this.updater.isAddNoCacheQuery)
+    let channelUrl = newUrlFromBase(channelFile, this.baseUrl, this.updater.isAddNoCacheQuery)
+    // channelUrl = await resolveIPUrl(channelUrl)
+    const dnsResolver = await dnsResolverManager()
+    if (dnsResolver) {
+      const ip = await dnsResolver.resolve(channelUrl.host)
+      if (ip) {
+        channelUrl = new HttpDnsURL('', channelUrl, {
+          ip: ip
+        })
+        this.ipCache[channelUrl.host] = ip
+      }
+    }
     for (let attemptNumber = 0; ; attemptNumber++) {
       try {
-        return parseUpdateInfo(await this.httpRequest(channelUrl), channelFile, channelUrl)
+        return parseUpdateInfo(await this.httpRequest(channelUrl, { host: channelUrl.hostname }), channelFile, channelUrl)
       }
       catch (e) {
         if (e instanceof HttpError && e.statusCode === 404) {
@@ -43,8 +61,40 @@ export class GenericProvider extends Provider<UpdateInfo> {
       }
     }
   }
-
+  get fileExtraDownloadHeaders(): OutgoingHttpHeaders | null {
+    return { host: this.baseUrl.hostname }
+  }
+  protected httpRequest(url: URL, headers?: OutgoingHttpHeaders | null, cancellationToken?: CancellationToken) {
+    const options = this.createRequestOptions(url, { ...headers, host: url.host || url.hostname })
+    return this.executor.request(options, cancellationToken)
+  }
   resolveFiles(updateInfo: UpdateInfo): Array<ResolvedUpdateFileInfo> {
-    return resolveFiles(updateInfo, this.baseUrl)
+    const file = resolveFiles(updateInfo, this.baseUrl).map(info => {
+      let url = info.url
+      const ip = this.ipCache[url.host]
+      if (ip) {
+        url = new HttpDnsURL('', url, {
+          ip
+        })
+      }
+      return {
+        ...info,
+        url
+      }
+    })
+    const packages = (updateInfo as WindowsUpdateInfo).packages
+    const packageInfo = packages == null ? null : (packages[process.arch] || packages.ia32)
+    if (packageInfo != null) {
+      const file0 = file[0] as any;
+      const file0url = new HttpDnsURL(file0.path, undefined);
+      if (this.ipCache[file0url.host]) {
+        file0url.ip = this.ipCache[file0url.host]
+      }
+      file0.packageInfo = {
+        ...packageInfo,
+        path: file0url.href
+      }
+    }
+    return file
   }
 }
